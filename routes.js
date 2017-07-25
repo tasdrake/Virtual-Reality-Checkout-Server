1dcb90a9d190d1ec794b46ed7c935bb6754de9df
Sean Eichenberger <sean.eich@gmail.com>
2017-07-25 17:23:24 -0600

    changed amount to req.body

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ router.post('/', (req,res,next)=>{
   console.log(req.body)
   var token = req.body.id; // Using Express
   var charge = stripe.charges.create({
-    amount: 1000,
+    amount: req.body.amount * 100,
     currency: "usd",
     description: "Example charge",
     receipt_email: req.body.email,

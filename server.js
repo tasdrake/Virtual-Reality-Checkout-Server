6489355db276194f9677952e89fd5d8f0c78f8a7
Sean Eichenberger <sean.eich@gmail.com>
2017-07-25 10:12:18 -0600

    working server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const stripe = require ("stripe")("sk_test_qqZDHIAgBZFr8pIlFLFQAOna")
+const routes = require('./routes.js');
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, PUT" )
+  next();
+});
+
+app.use(bodyParser.json())
+app.use(routes)
+
+app.listen(3000, ()=>{
+  console.log('Listening on port 3000');
+});
+
+module.exports = app;

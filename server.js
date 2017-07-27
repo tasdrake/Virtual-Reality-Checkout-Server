f6563e1608a6d780bece701feee7e6ba6cdfd74d
Sean Eichenberger <sean.eich@gmail.com>
2017-07-27 08:14:12 -0600

    rewind to socket only

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const stripe = require ("stripe")("sk_test_qqZDHIAgBZFr8pIlFLFQAOna")
-const routes = require('./routes.js');
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const app = express();
+// const stripe = require ("stripe")("sk_test_qqZDHIAgBZFr8pIlFLFQAOna")
+// const routes = require('./routes.js');
+// const server = require('http').Server(app);
+// const io = require('socket.io')(server);
 
 ///////////////////////////////this code works start
-// const express = require('express');
-// const socketIO = require('socket.io');
-// const PORT = process.env.PORT || 3000;
+const express = require('express');
+const socketIO = require('socket.io');
+const PORT = process.env.PORT || 3000;
 //////////////////except this start
 // const app = express()
 // app.listen(3003)
 ////////////except this end
-// const server = express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
-//const io = socketIO(server);
+const server = express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const io = socketIO(server);
 
   io.on('connection', function(socket){
     console.log('a user connected c');
@@ -24,20 +24,20 @@ const io = require('socket.io')(server);
 ///////////////this code works end
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, PUT" )
-  next();
-});
-app.use(bodyParser.json())
-app.use(routes)
-
-//
-// http.listen(6000, function(){
-//   console.log('listening on *:6000');
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, PUT" )
+//   next();
 // });
+// app.use(bodyParser.json())
+// app.use(routes)
 //
-//app.listen(process.env.PORT || 5000)
+// //
+// server.listen(6000, function(){
+//    console.log('listening on *:6000');
+// });
+// //
+// app.listen(process.env.PORT || 5000)
 
 module.exports = {app:app, server:server};

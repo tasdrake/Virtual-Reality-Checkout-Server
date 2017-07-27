25467f8307801eabb1bd427de32c48b1e806511c
Sean Eichenberger <sean.eich@gmail.com>
2017-07-26 20:04:41 -0600

    deleted app.uses

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ const io = require('socket.io')(http);
   });
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, PUT" )
-  next();
-});
-app.use(bodyParser.json())
-app.use(routes)
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, PUT" )
+//   next();
+// });
+// app.use(bodyParser.json())
+// app.use(routes)
 
 
 http.listen(6000, function(){

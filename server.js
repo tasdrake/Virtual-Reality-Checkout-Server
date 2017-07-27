80cc11330dce76d6661b9314b84aad348ff764c6
Sean Eichenberger <sean.eich@gmail.com>
2017-07-26 22:24:36 -0600

    github ex

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-// const express = require('express');
- const bodyParser = require('body-parser');
-// const app = express();
-// const server = app.listen(8080)
-//const stripe = require ("stripe")("sk_test_qqZDHIAgBZFr8pIlFLFQAOna")
-//const routes = require('./routes.js');
-//const http = require('http').Server(app);
-//const io = require('socket.io')(http);
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const stripe = require ("stripe")("sk_test_qqZDHIAgBZFr8pIlFLFQAOna")
+const routes = require('./routes.js');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 ///////////////////////////////this code works start
 // const express = require('express');
@@ -16,16 +15,14 @@
 // app.listen(3003)
 ////////////except this end
 // const server = express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
-// const io = socketIO(server);
-//
-//   io.on('connection', function(socket){
-//     console.log('a user connected c');
-//     socket.emit('amount', 'pleasework');
-//   });
+//const io = socketIO(server);
+
+  io.on('connection', function(socket){
+    console.log('a user connected c');
+    socket.emit('amount', 'pleasework');
+  });
 ///////////////this code works end
-var Express = require('express'),
-  app = Express.createServer(),
-	io = require('socket.io').listen(app);
+
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -43,4 +40,4 @@ app.use(routes)
 //
 //app.listen(process.env.PORT || 5000)
 
-module.exports = server;
+module.exports = {app:app, server:server};

597499ee6b61610e1ac26e1cbc4764f630cc117b
Sean Eichenberger <sean.eich@gmail.com>
2017-07-26 20:38:22 -0600

    stack overflow

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const server = app.listen(8080)
 //const stripe = require ("stripe")("sk_test_qqZDHIAgBZFr8pIlFLFQAOna")
 //const routes = require('./routes.js');
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-
+//const http = require('http').Server(app);
+//const io = require('socket.io')(http);
+const io = require('socket.io').listen(server)
   io.on('connection', function(socket){
     console.log('a user connected c');
     socket.emit('amount', 'pleasework');
@@ -26,6 +27,6 @@ http.listen(6000, function(){
   console.log('listening on *:6000');
 });
 //
-app.listen(process.env.PORT || 5000)
+//app.listen(process.env.PORT || 5000)
 
 module.exports = app;

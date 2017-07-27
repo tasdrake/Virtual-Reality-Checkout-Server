b559c85ce6cbea20b8f50067c337d919ecd1dcb4
Sean Eichenberger <sean.eich@gmail.com>
2017-07-26 21:49:39 -0600

    added back use

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 // const app = express();
 // const server = app.listen(8080)
 //const stripe = require ("stripe")("sk_test_qqZDHIAgBZFr8pIlFLFQAOna")
@@ -21,14 +21,14 @@ const io = socketIO(server);
 
 
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, PUT" )
-//   next();
-// });
-// app.use(bodyParser.json())
-//app.use(routes)
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, PUT" )
+  next();
+});
+server.use(bodyParser.json())
+server.use(routes)
 
 //
 // http.listen(6000, function(){
